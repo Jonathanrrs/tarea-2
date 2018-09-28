7c73727ec03a151765110f4988fc2877a815e1f9
Jonathan Ramiro Ruiz Sandoval <yoni_12rrs@hotmail.com>
2018-09-27 21:36:26 -0500

    distancia

---
+++ ejercicio-4/ejercicio-4.js
@@ -0,0 +1,14 @@
+// Calcular Distancia
+var btnCalcularDistancia = document.querySelector('#calcularDistancia');
+btnCalcularDistancia.addEventListener('click', distancia);
+
+
+function distancia() {
+    var inputNumero1 = document.querySelector('#numero1');
+    var inputNumero2 = document.querySelector('#numero2');
+
+    var resultado = Number(inputNumero1.value) * Number(inputNumero2.value);
+
+    var inputResultado = document.querySelector('#resultado');
+    inputResultado.value = resultado;
+}
